117c21cef0096039cd6a7310a3413b8a2f8ebb63
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-28 23:58:29 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,7 +23,11 @@ end
 def literal_aoh
   
   literal_aoh = [
-    
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+    { name:  "Pat Monteseian", occupation: "Staff" },
+    { name:  "Kate Gompert", occupation: "None" },
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
   
   # Using Array literal syntax only, build a nested array that uses the data in
