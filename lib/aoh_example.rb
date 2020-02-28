230f4d9d27061cf6f1368973ef0f05910bc2b208
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-28 23:58:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,6 +21,11 @@ def assembled_aoh
 end
 
 def literal_aoh
+  
+  literal_aoh = [
+    
+    ]
+  
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
