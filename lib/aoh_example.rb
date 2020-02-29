59e14f8aff83f444d8419d4fe912df40c7a31f8e
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-02-29 00:06:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -39,6 +39,11 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
+  
+  aoh[row][key] = new_value
+  
+  return aoh
+  
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
